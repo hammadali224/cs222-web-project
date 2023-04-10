140ee6d61fb56650ecce4d03bd49c552b86136b1
Vijwal Rao Akula <93107214+vijwal0804@users.noreply.github.com>
2023-04-09 19:10:22 -0500

    Improved runtime for the main JS function
    Reduced the lines of code from 12 all the way to 6, which would help improve the runtime for our project. I used the built in Math object to calculate the value of a base raised to the power of an exponent. Earlier I used a for loop for exponents which had O(n) runtime, where n was the age difference between current and desired age. Now, this has been reduced to O(1) runtime. I also got rid of unnecessary constants.

--- Projectmainmethod.js
+++ Projectmainmethod.js
@@ -2,16 +2,10 @@
 
 
 function retirementsavingscalc(income, expenditure, tax, div, roi, inf, retireage, presentage) {
-let principle = (expenditure * 120000) / ((100 * div) - (tax * div));
-let realroi = (100 + roi) / (100 + inf);
-let num = realroi;
-let n = retireage - presentage;
-for (let i = 1; i < n; i++) {
-realroi = realroi * num;
-}
-let geometricprogression = (roi - 1) / (num - 1);
-let savings = principle / geometricprogression;
-let monthlysavings = savings / 12;
-let intpercent = (monthlysavings / income) * 100;
-console.log(`You have to save $${monthlysavings.toFixed(2)} of your monthly income, which is about ${intpercent.toFixed(2)}% to retire at the age of your choice`);
+  const principle = (expenditure * 120000) / ((100 * div) - (tax * div));
+  const realroi = Math.pow((100 + roi) / (100 + inf), retireage - presentage);
+  const savings = principle * (roi - 1) / ((100 + roi) / (100 + inf) - 1);
+  const monthlysavings = savings / 12;
+  const intpercent = (monthlysavings / income) * 100;
+  console.log(`You have to save $${monthlysavings.toFixed(2)} of your monthly income, which is about ${intpercent.toFixed(2)}% to retire at the age of your choice`);
 }

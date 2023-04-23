25722af246241a3e8893a50a06e033b0ed90feb5
Vijwal Rao Akula <93107214+vijwal0804@users.noreply.github.com>
2023-04-22 23:48:15 -0500

    Eliminating negative results
    Introducing x and y will prevent cases that show negative numbers as savings which is logically not possible. If current savings invested are enough to retire when the time comes, the algorithm shall output : Congratulations! You have achieved Financial freedom. Your savings invested are enough to fund your retirement.

--- Projectmainmethod.js
+++ Projectmainmethod.js
@@ -10,8 +10,15 @@ function retirementsavingscalc(income, expenditure, currsavings, tax, div, roi, 
     console.log("Error: Retireage must be greater than presentage.");
     return;
   }
+  
   const principle = (expenditure * 10000) / ((100 * div) - (tax * div));
   const exproi = Math.pow((100 + roi) / (100 + inf), retireage - presentage);
+  const x = (currsavings) * (exproi);
+  const y = (expenditure) * (100/div) * (100 / (100 - tax)) * 12;
+  if (x >= y) {
+    console.log("Congratulations! You have achieved Financial freedom. Your savings invested are enough to fund your retirement");
+    return;
+  }
   const currentsavingendval = currsavings * exproi;
   const savings = (principle - currentsavingendval) * (roi - 1) / (exproi - 1);
   const intpercent = (savings / income) * 100;
@@ -20,23 +27,31 @@ function retirementsavingscalc(income, expenditure, currsavings, tax, div, roi, 
 
 // Test Case 1
 retirementsavingscalc(10000, 8000, 30000, 10, 12, 6, 3, 45, 25);
-//Output: You have to save $3136.57 of your monthly income, which is about 31.37% to retire at the age of your choice.
+//Output: You have to save $3136.57 of your monthly income, which is about 31.37% to retire at the age of your choice
 
 // Test Case 2
 retirementsavingscalc(12000, 9000, 25000, 15, 12, 5, 2, 50, 30);
-//Output: You have to save $3293.25 of your monthly income, which is about 27.44% to retire at the age of your choice.
+//Output: You have to save $3293.25 of your monthly income, which is about 27.44% to retire at the age of your choice
 
 // Test Case 3
 retirementsavingscalc(7000, 6000, 40000, 20, 12, 4, 2, 55, 35);
-//You have to save $2035.83 of your monthly income, which is about 29.08% to retire at the age of your choice.
+//Output: You have to save $2035.83 of your monthly income, which is about 29.08% to retire at the age of your choice
 
 // Test Case 4
 retirementsavingscalc(10000, 8000, 50000, 5, 12, 7, 3, 60, 40);
-//You have to save $4193.27 of your monthly income, which is about 41.93% to retire at the age of your choice
+//Output: You have to save $4193.27 of your monthly income, which is about 41.93% to retire at the age of your choice
 
 // Test Case 5
 retirementsavingscalc(15000, 12000, 30000, 8, 12, 6, 2, 65, 45);
-//You have to save $5301.34 of your monthly income, which is about 35.34% to retire at the age of your choice
+//Output: You have to save $5301.34 of your monthly income, which is about 35.34% to retire at the age of your choice
+
+//Test Case 6
+retirementsavingscalc(15000, 5000, 1000000, 20, 8, 8, 2, 65, 40);
+//Output: Congratulations! You have achieved Financial freedom. Your savings invested are enough to fund your retirement
+
+//Test Case 7
+retirementsavingscalc(10000, 4000, 700000, 20, 5, 12, 2, 60, 30);
+//Output: Congratulations! You have achieved Financial freedom. Your savings invested are enough to fund your retirement
 
 //All Tests cases gave the desired Output, as they matched the Outputs of other similar apps with 100% success rate.
 
